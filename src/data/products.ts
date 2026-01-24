4533f70379a221d4d814a73d2be7ab9a0a427b46
Krishna Narayan Singh <krishnanarayansingh65@gmail.com>
2026-01-24 13:30:51 +0530

    Update

--- src/data/products.ts
+++ src/data/products.ts
@@ -81,7 +81,7 @@ export const products: Product[] = [
   {
     id: "6",
     name: "Glow Face Pack",
-    price: 38.00,
+    price: 200.00,
     image: "/images/Glow-face-pack.jpeg",
     category: "Makeup",
     rating: 4.5,
@@ -91,7 +91,7 @@ export const products: Product[] = [
   {
     id: "7",
     name: "Herbal D-Tan Clenser Powder",
-    price: 22.00,
+    price: 150.00,
     image: "/images/Herbal D-Tan Clenser Powder.jpeg",
     category: "Body Care",
     rating: 4.4,
@@ -100,9 +100,9 @@ export const products: Product[] = [
   },
   {
     id: "8",
-    name: "Rose Mist Toner",
-    price: 26.00,
-    image: "https://images.unsplash.com/photo-1629380108574-40c083555579?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxiZWF1dHklMjBzcGElMjBwcm9kdWN0c3xlbnwxfHx8fDE3NjMyMTg1MDN8MA&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral",
+    name: "Body Butter",
+    price: 249.00,
+    image: "/images/Body Butter.jpeg",
     category: "Skincare",
     rating: 4.7,
     description: "Refresh and balance your skin with rosewater.",
